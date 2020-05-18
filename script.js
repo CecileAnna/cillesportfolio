0b04db5045069626ad2fafab7c69398c25cef6af
CecileAnna <65543962+CecileAnna@users.noreply.github.com>
2020-05-18 14:06:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var galleryElements = document.querySelectorAll("#gallery img"); 
+var lb = document.querySelector(".lb-container"); 
+var lbImg = document.querySelector(".lb-image"); 
+var lbClose = document.querySelector(".lb-close"); 
+
+ 
+ 
+
+galleryElements.forEach(function(el){ 
+    el.addEventListener("click", function(){ 
+        var src = el.getAttribute("src"); 
+        lbImg.style.backgroundImage = "url("+src+")"; 
+        lb.classList.add("lb-container-active"); 
+    }); 
+});  
+
+ 
+ 
+
+lbClose.addEventListener("click", function(){ 
+    lbImg.style.backgroundImage = ""; 
+    lb.classList.remove("lb-container-active"); 
+}); 
